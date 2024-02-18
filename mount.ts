cd27d89a6ad974c4ea477dfcd6014b1fb1e7e1e3
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2024-02-18 15:55:13 +0100

    Minor refactor mount function

--- mount.ts
+++ mount.ts
@@ -1,40 +1,39 @@
 import { getContainerEl, setupHooks } from '@cypress/mount-utils';
 import { type VNode } from '@stencil/core';
 
-export function mount(template: VNode | VNode[] | string, options: Options = { waitUntilVisible: true }) {
+export function mount(
+    template: VNode | VNode[] | string,
+    options: Options = { waitUntilVisible: Cypress.env('CtStencilWaitUntilVisible'), log: Cypress.env('CtStencilLog') }
+) {
     const container = getContainerEl();
-
-    if (typeof template === 'string') {
-        container.innerHTML = template;
-    } else if (Array.isArray(template)) {
+    if (Array.isArray(template)) {
         template.forEach((v) => {
-            const element = createDOMNode(v);
-            container.appendChild(element);
+            const element = createDOMNode(v, options.log);
+            element && container.appendChild(element);
         });
+    } else if (typeof template === 'string') {
+        container.innerHTML = template;
     } else {
-        const element = createDOMNode(template);
-        container.appendChild(element);
-        options.waitUntilVisible && cy.get(element.nodeName.toLowerCase(), { log: false }).should('be.visible').as(element.nodeName.toLowerCase());
+        const element = createDOMNode(template, options.log);
+        element && container.appendChild(element);
     }
 
-    // adds output to the command log
-    if (options.log) {
-        return cy.wait(0, { log: false }).then(() => {
-            Cypress.log({
-                name: 'mount',
-                message: 'Mounted component',
-            });
-        });
-    }
+    if (options.waitUntilVisible) Array.from(container.children).forEach((v) => waitUntilVisible(v));
+}
+
+function waitUntilVisible(element: Element | undefined) {
+    if (!element) return;
+    if ('children' in element) Array.from(element.children).forEach((v) => waitUntilVisible(v));
+    cy.get(element.nodeName.toLowerCase()).should('be.visible');
 }
 
 /**
  * Function that recusively creates DOM nodes.
  * @param vnode Stencil's VNode
- * @returns - Node
  */
-function createDOMNode(vnode: VNode): Node {
+function createDOMNode(vnode: VNode, log: boolean | undefined): Node | void {
     const { $attrs$: attrs, $tag$: tag } = vnode;
+    if (!tag) return;
     const el = document.createElement(tag as string);
 
     for (const key in attrs) {
@@ -62,10 +61,12 @@ function createDOMNode(vnode: VNode): Node {
 
     for (const child of children) {
         const text = child.$text$;
-        const childNode = createDOMNode(child);
+        const childNode = createDOMNode(child, log);
         if (childNode) el.appendChild(childNode);
         if (text) el.innerText = text;
     }
+
+    if (log) Cypress.log({ name: 'Mount', message: 'Mounting node ' + el.tagName.toLowerCase() });
 
     return el;
 }
@@ -84,7 +85,14 @@ function cleanup() {
 setupHooks(cleanup);
 
 interface Options {
+    /**
+     * Waits for all children in the mount function to be visible.
+     * @notice does not wait for children in `shadowRoot`.
+     */
     waitUntilVisible?: boolean;
+    /**
+     * Logs when its about to mount a node.
+     */
     log?: boolean;
 }
 
