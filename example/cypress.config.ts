3e0f879bc300027afdd27df3482905ac2cfb4433
CoderIllusionist <75251048+CoderIllusionist@users.noreply.github.com>
2024-02-18 15:55:23 +0100

    Added env variables

--- example/cypress.config.ts
+++ example/cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'cypress';
 import { defineConfig as defineConfigVite } from 'vite';
 
 export default defineConfig({
+  env: {
+    CtStencilWaitUntilVisible: true,
+    CtStencilLog: false,
+  },
   component: {
     devServer: {
       framework: 'cypress-ct-stencil' as any,
